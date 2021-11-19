9ba7739b6bf3ead337a22ab0d13e0793d324e322
Michael Koontz <mikoontz@gmail.com>
2021-11-19 08:47:56 -0700

    pull in IDTrees data for a case study

---
+++ workflow/collate-idtrees-data.R
@@ -0,0 +1,5 @@
+library(data.table)
+
+# Ben Weinstein, Sergio Marconi, Alina Zare, Stephanie Bohlman, Sarah Graves, Aditya Singh, & Ethan White. (2020). NEON Tree Crowns Dataset (0.0.1) [Data set]. Zenodo. https://doi.org/10.5281/zenodo.3765872
+# https://doi.org/10.5281/zenodo.3765871
+f <- fread("data/raw/idtrees/NIWO_2019.csv")
