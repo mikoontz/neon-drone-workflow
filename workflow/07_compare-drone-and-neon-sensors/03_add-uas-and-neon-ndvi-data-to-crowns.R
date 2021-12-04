5948384ce399283e80f9682747366f052ffdf6c2
Michael Koontz <mikoontz@gmail.com>
2021-12-04 15:08:51 -0800

    create Level 3b data to compare NDVI between individual tree crowns

---
+++ workflow/07_compare-drone-and-neon-sensors/03_add-uas-and-neon-ndvi-data-to-crowns.R
@@ -0,0 +1,43 @@
+# Extract NDVI from within each tree crown segment
+
+library(dplyr)
+library(sf)
+library(terra)
+library(exactextractr)
+
+site_name <- "niwo_017"
+flight_datetime <- "2019-10-09"
+
+# files to be read
+cropped_crowns_fname <- file.path("data", "drone", "L3a", "geometric", site_name, flight_datetime, paste0(site_name, "_", flight_datetime, "_crowns_cropped.gpkg"))
+gcp_locations_fname <- paste0("data/out/", site_name, "_gcp-locations.gpkg")
+ndvi_drone_fname <- file.path("data", "drone", "L3a", "spectral", site_name, flight_datetime, paste0(site_name, "_", flight_datetime, "_ndvi.tif"))
+ndvi_neon_fname <- file.path("data", "out", "AOP", "ndvi_spectral-resampled.tif")
+
+# files to write
+augmented_crowns_fname <- file.path("data", "drone", "L3b", paste0(site_name, "_", flight_datetime, "_", "crowns-with-reflectance_cropped.gpkg"))
+
+gcp <- sf::st_read(gcp_locations_fname)
+crowns_raw <- sf::st_read(cropped_crowns_fname) %>% dplyr::rename(geometry = geom)
+drone_ndvi <- terra::rast(ndvi_drone_fname)
+neon_resampled_ndvi <- terra::rast(ndvi_neon_fname)
+
+crowns_with_neon_ndvi <- 
+  exactextractr::exact_extract(x = neon_resampled_ndvi, y = crowns_raw,
+                                                      fun = c("mean", "stdev"),
+                                                      append_cols = "treeID") %>% 
+  dplyr::rename(ndvi_mean_neon = mean,
+                ndvi_stdev_neon = stdev)
+
+crowns_with_uas_ndvi <- 
+  exactextractr::exact_extract(x = drone_ndvi, y = crowns_raw, 
+                                                     fun = c("mean", "stdev"),
+                                                     append_cols = "treeID") %>% 
+  dplyr::rename(ndvi_mean_uas = mean, ndvi_stdev_uas = stdev)
+
+crowns <- 
+  dplyr::left_join(x = crowns_with_neon_ndvi, y = crowns_with_uas_ndvi, by = "treeID") %>% 
+  dplyr::left_join(crowns_raw, by = "treeID") %>% 
+  sf::st_as_sf()
+
+sf::st_write(obj = crowns, dsn = augmented_crowns_fname)
